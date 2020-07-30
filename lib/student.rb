189754ce10168175e494e73e6b3575709f65bec0
Travis Fox <travis.fox90@gmail.com>
2020-07-30 01:01:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,11 @@ require_relative "./user.rb"
 class Student < User
   attr_accessor :knowledge
   
+  @@all = []
   
   def initialize
     @knowledge = []
+    @@all << self
   end
   
   def learn(knowledge)
