fa6570aa99ee5fac02b55ee10b75f4d8267e5ea1
Travis Fox <travis.fox90@gmail.com>
2020-07-30 01:07:06 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,25 @@
-class Student 
+require_relative "./user.rb"
 
+class Student < User
+  attr_accessor :knowledge
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize
+    @knowledge = []
+    @@all << self
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
+  
+  def knowledge
+    @knowledge
+  end
+  
 end
